1b72ba9dbf22b87ddd6e28e8a78641ef6c223e09
scarsed <kimaniwilber@gmail.com>
2025-12-06 01:44:42 +0300

    Update twrp_CLA6.mk

--- twrp_CLA6.mk
+++ twrp_CLA6.mk
@@ -6,7 +6,7 @@
 #
 
 # Inherit from TECNO-CLA6 device
-$(call inherit-product, device/tecno/tecno/CLA6/device.mk)
+$(call inherit-product, device/tecno/CLA6/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
