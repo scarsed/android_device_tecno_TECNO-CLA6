669f94817fc64ad7a0ca31558151a4eaeb13eacd
scarsed <kimaniwilber@gmail.com>
2025-12-05 23:06:16 +0300

    Clone TWRP device tree for transsion mt6789

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,1 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo omni_TECNO-CLA6-user
-add_lunch_combo omni_TECNO-CLA6-userdebug
-add_lunch_combo omni_TECNO-CLA6-eng
+git clone https://github.com/transsion-mt6789/twrp-device_transsion_mt6789-common.git -b fox_12.1 device/transsion/mt6789-common
